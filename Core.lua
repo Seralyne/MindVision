5803a6b82bfb120ebc304bb1a91a8521fab33a7e
Seralyne <serverboost100.dk@gmail.com>
2025-10-19 15:51:27 +0200

    Very specific prototype implemented. All that remains is to generalise this and make it easily configurable and persistent. Oh and a nice UI wouldn't hurt either

--- Core.lua
+++ Core.lua
@@ -1,10 +1,8 @@
-MindVision = LibStub("AceAddon-3.0"):NewAddon("MindVision", "AceConsole-3.0") -- Add extra mixins as you need them
+MindVision = LibStub("AceAddon-3.0"):NewAddon("MindVision", "AceConsole-3.0", "AceEvent-3.0") -- Add extra mixins as you need them
+
+--MindVision:RegisterEvent("ZONE_CHANGED_NEW_AREA")
 
 
-function MindVision:OnInitialize()
-    self.db = LibStub("AceDB-3.0"):New("MindVisionDB")
-    MindVision:Print("Mind Vision Gamma Addon Loaded!")
-end
 
 function MindVision:GetMessage(info)
     return messageVar
@@ -13,6 +11,29 @@ end
 function MindVision:SetMessasge(info, input)
     messageVar = input
 end
+
+function MindVision:ZONE_CHANGED_NEW_AREA()
+    local zone =  GetZoneText()
+    self:Print("Zone Changed to " .. zone)
+
+
+    -- This is a crude solution. But it works for testing purposes. 
+    if zone == "The Great Sea" then
+        SetCVar("gamma", 2.8)
+        self:Print("Gamma Set to 2.8")
+    else
+        SetCVar("gamma", 1.0)
+        self:Print("Gamma Set to 1.0")
+    end
+
+end
+
+function MindVision:OnInitialize()
+    self.db = LibStub("AceDB-3.0"):New("MindVisionDB")
+    self:RegisterEvent("ZONE_CHANGED_NEW_AREA")
+    self:Print("Mind Vision Gamma Addon Loaded!")
+end
+
 
 
 local options = {
@@ -45,3 +66,4 @@ local options = {
 
 LibStub("AceConfig-3.0"):RegisterOptionsTable("MindVision", options, {"getmessage", "setmessage"})
 LibStub("AceConfigDialog-3.0"):AddToBlizOptions("MindVision","MindVision")
+
